cae40d73191804101a3e22b8b0916186d9574162
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-04-03 19:13:53 -0300

    create attendee list component

--- components/attendee-list.tsx
+++ components/attendee-list.tsx
@@ -1,3 +1,100 @@
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+  MoreHorizontal,
+  Search,
+} from 'lucide-react'
+import { IconButton } from '@/components/icon-button'
+import { Table } from '@/components/table/table'
+import { TableHeader } from '@/components/table/table-header'
+import { TableCell } from '@/components/table/table-cell'
+import { TableRow } from './table/table-row'
+
 export function AttendeeList() {
-  return <div>attendee-list</div>
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-3">
+        <h1 className="text-2xl font-bold">Participantes</h1>
+        <div className="px-3 py-1.5 w-72 border border-white/10 rounded-lg text-sm flex items-center gap-3">
+          <Search className="size-4 text-emerald-300" />
+          <input
+            type="text"
+            placeholder="Buscar participantes..."
+            className="bg-transparent outline-none flex-1 p-0 border-0 text-sm"
+          />
+        </div>
+      </div>
+      <Table>
+        <thead>
+          <tr className="border-b border-white/10">
+            <TableHeader className="w-12">
+              <input
+                type="checkbox"
+                className="size-4 bg-black/20 rounded border border-white/10 "
+              />
+            </TableHeader>
+            <TableHeader>Código</TableHeader>
+            <TableHeader>Participantes</TableHeader>
+            <TableHeader>Data de inscrição</TableHeader>
+            <TableHeader>Data do check-in</TableHeader>
+            <TableHeader className="w-16"></TableHeader>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from({ length: 18 }).map((_, i) => (
+            <TableRow key={i}>
+              <TableCell>
+                <input
+                  type="checkbox"
+                  className="size-4 bg-black/20 rounded border border-white/10 "
+                />
+              </TableCell>
+              <TableCell>198273</TableCell>
+              <TableCell>
+                <div className="flex flex-col gap-1">
+                  <span className="font-semibold text-zinc-50">
+                    Nome Da pessoa
+                  </span>
+                  <span>email@email.com</span>
+                </div>
+              </TableCell>
+              <TableCell>7 dias atrás</TableCell>
+              <TableCell>3 dias atrás</TableCell>
+              <TableCell>
+                <IconButton transparent>
+                  <MoreHorizontal className="size-4" />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <TableCell colSpan={3}>Mostrando 10 de 228 itens</TableCell>
+            <TableCell colSpan={3}>
+              <div className="flex items-center gap-8 justify-end">
+                <span>Pagina 1 de 42</span>
+                <div className="flex gap-1.5">
+                  <IconButton>
+                    <ChevronsLeft className="size-4" />
+                  </IconButton>
+                  <IconButton>
+                    <ChevronLeft className="size-4" />
+                  </IconButton>
+                  <IconButton>
+                    <ChevronRight className="size-4" />
+                  </IconButton>
+                  <IconButton>
+                    <ChevronsRight className="size-4" />
+                  </IconButton>
+                </div>
+              </div>
+            </TableCell>
+          </tr>
+        </tfoot>
+      </Table>
+    </div>
+  )
 }
