33db0a05b93e842dca240298b701a88a8f9a6600
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-04-03 19:14:00 -0300

    create header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,17 @@
+import Image from 'next/image'
+import icon from '@/assets/icon.svg'
+import { NavLink } from './nav-link'
+
 export function Header() {
-  return <div>header</div>
+  return (
+    <header className="flex items-center gap-5 py-2">
+      <Image alt="icon </>" src={icon} />
+      <nav className="flex items-center gap-5">
+        <NavLink href="/eventos" active>
+          Eventos
+        </NavLink>
+        <NavLink href="/">Participantes</NavLink>
+      </nav>
+    </header>
+  )
 }
